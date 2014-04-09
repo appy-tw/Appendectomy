fff1e78ead00b295c518e47ef7d14e2622292f00
sfhuang <huang.shengfeng@gmail.com>
2014-04-09 13:41:37 +0800

    New Function: Validation of ID Number

--- appendectomy/input_petition_form.php
+++ appendectomy/input_petition_form.php
@@ -34,7 +34,7 @@
 			<TABLE STYLE='BACKGROUND-COLOR:#DDDDDD'>
 			<TR><TD STYLE='WIDTH:80;TEXT-ALIGN:CENTER'>1</TD>
 				<TD>姓　　　名</TD><TD><INPUT TYPE=TEXT NAME=Name_0 STYLE='WIDTH:100;FONT-SIZE:16'></TD>
-				<TD>身分證字號</TD><TD><INPUT TYPE=TEXT NAME=IDNo_0 STYLE='WIDTH:150;FONT-SIZE:16'></TD></TR>
+				<TD>身分證字號</TD><TD><INPUT TYPE=TEXT NAME=IDNo_0 STYLE='WIDTH:150;FONT-SIZE:16'></TD><TD><IMG SRC='info.png' STYLE='WIDTH:20;CURSOR:POINTER' ALT='請輸入半形英文數字' TITLE='請輸入半形英文數字'></TD></TR>
 			<TR><TD></TD>
 				<TD>性　　　別</TD><TD><INPUT TYPE=RADIO NAME=Sex_0 VALUE='M' CHECKED>男&nbsp;<INPUT TYPE=RADIO NAME=Sex_0 VALUE='F'>女</TD>
 				<TD>出生年月日</TD><TD>";
@@ -96,6 +96,7 @@
 	}
 php?>
 <SCRIPT>
+//動態增加表單
 function ADDFORM()
 {
 	var no=parseInt(document.getElementById('Size').value);
@@ -134,7 +135,7 @@ function ADDFORM()
 			'<TABLE STYLE=\'BACKGROUND-COLOR:'+bgcolor+'\'>'+
 			'<TR><TD STYLE=\'WIDTH:80;TEXT-ALIGN:CENTER\'>'+(no+1)+'</TD>'+
 				'<TD>姓　　　名</TD><TD><INPUT TYPE=TEXT NAME=Name_'+no+' STYLE=\'WIDTH:100;FONT-SIZE:16\'></TD>'+
-				'<TD>身分證字號</TD><TD><INPUT TYPE=TEXT NAME=IDNo_'+no+' STYLE=\'WIDTH:150;FONT-SIZE:16\'></TD></TR>'+
+				'<TD>身分證字號</TD><TD><INPUT TYPE=TEXT NAME=IDNo_'+no+' STYLE=\'WIDTH:150;FONT-SIZE:16\'></TD><TD><IMG SRC=\'info.png\' STYLE=\'WIDTH:20;CURSOR:POINTER\' ALT=\'請輸入半形英文數字\' TITLE=\'請輸入半形英文數字\'></TD></TR>'+
 			'<TR><TD></TD>'+
 				'<TD>性　　　別</TD><TD><INPUT TYPE=RADIO NAME=Sex_'+no+' VALUE=\'M\' CHECKED>男&nbsp;<INPUT TYPE=RADIO NAME=Sex_'+no+' VALUE=\'F\'>女</TD>'+
 				'<TD>出生年月日</TD><TD>'+returnB_Y(no)+returnB_M(no)+returnB_D(no)+'</TD><TD><IMG SRC=\'info.png\' STYLE=\'WIDTH:20;CURSOR:POINTER\' ALT=\'必須在 1992 年 1 月 13 日（含）以前才能連署罷免\' TITLE=\'必須在 1992 年 1 月 13 日（含）以前出生才能連署或連署罷免\'></TD></TR>'+
@@ -163,6 +164,7 @@ function ADDFORM()
 	}
 }
 
+//複製戶籍地址
 function filladd(no)
 {
 	if(document.getElementById('SAMEADD_'+no).checked)
@@ -171,6 +173,7 @@ function filladd(no)
 		document.getElementsByName('RegAdd_'+no)[0].value='';
 }
 
+//年份選單
 function returnB_Y(no)
 {
 	var currentYear= new Date().getFullYear();
@@ -184,6 +187,7 @@ function returnB_Y(no)
 	return rtnStr;
 }
 
+//月份選單
 function returnB_M(no)
 {
 	rtnStr='<SELECT NAME=\'Birthday_m_'+no+'\' STYLE=\'WIDTH:60;FONT-SIZE:16\'>';
@@ -195,6 +199,7 @@ function returnB_M(no)
 	return rtnStr;
 }
 
+//日期選單
 function returnB_D(no)
 {
 	rtnStr='<SELECT NAME=\'Birthday_d_'+no+'\' STYLE=\'WIDTH:60;FONT-SIZE:16\'>';
@@ -206,6 +211,7 @@ function returnB_D(no)
 	return rtnStr;
 }
 
+//輸入資訊辨識
 function checkInput()
 {
 	var warningStr='';
@@ -222,6 +228,10 @@ function checkInput()
 		if(document.getElementsByName('IDNo_'+i)[0].value=='')
 		{
 			warningStr+='編號 '+(i+1)+' 的身分證字號不可為空白\n';
+		}
+		if(idCheck(document.getElementsByName('IDNo_'+i)[0].value)==false)
+		{
+			warningStr+='編號 '+(i+1)+' 的身分證字號錯誤\n';
 		}
 		if(document.getElementsByName('Occupation_'+i)[0].value=='')
 		{
@@ -242,4 +252,35 @@ function checkInput()
 	}
 }
 
+//身分證辨識
+function idCheck(id)
+{
+	var idArray=new Array();
+	idArray[10]="A";	idArray[11]="B";	idArray[12]="C";	idArray[13]="D";
+	idArray[14]="E";	idArray[15]="F";	idArray[16]="G";	idArray[17]="H";
+	idArray[34]="I";	idArray[18]="J";	idArray[19]="K";	idArray[20]="L";
+	idArray[21]="M";	idArray[22]="N";	idArray[35]="O";	idArray[23]="P";
+	idArray[24]="Q";	idArray[25]="R";	idArray[26]="S";	idArray[27]="T";
+	idArray[28]="U";	idArray[29]="V";	idArray[30]="X";	idArray[31]="Y";
+	var newIdArray=idArray.indexOf(id.toUpperCase().substr(0,1))+id.substr(1,9);
+	var baseNumber=
+		parseInt(newIdArray.substr(0,1))*1+
+		parseInt(newIdArray.substr(1,1))*9+
+		parseInt(newIdArray.substr(2,1))*8+
+		parseInt(newIdArray.substr(3,1))*7+
+		parseInt(newIdArray.substr(4,1))*6+
+		parseInt(newIdArray.substr(5,1))*5+
+		parseInt(newIdArray.substr(6,1))*4+
+		parseInt(newIdArray.substr(7,1))*3+
+		parseInt(newIdArray.substr(8,1))*2+
+		parseInt(newIdArray.substr(9,1))*1;
+	if((baseNumber%10)==0)
+		residue=0;
+	else
+		residue=10-(baseNumber%10);
+	if(parseInt(newIdArray.substr(10,1))==residue)
+		return true;
+	else
+		return false;
+}
 </SCRIPT>
