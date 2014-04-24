22b5e9615d7bbdd7de1a8a2067da083f14cb6d3a
Ihc <ihcaoe@gmail.com>
2014-04-24 22:37:37 +0800

    移過去就註解掉 丞宏

--- appendectomy/proposalframe.php
+++ appendectomy/proposalframe.php
@@ -1,30 +1,30 @@
-<body onload=document.TRANSFER.submit();>
+<!-- <body onload=document.TRANSFER.submit();>-->
+已移到application/controller/GenPDF矣<br/>
+// <?php
+// // 	ECHO "<FORM NAME=TRANSFER ACTION='../index.php/appy/proposal' METHOD=POST TARGET=pdfframe>";
+// // 	while($element = current($_POST)) {
+// //     echo "<INPUT TYPE=HIDDEN NAME=".key($_POST)." VALUE='".$_POST[key($_POST)]."'>";
+// //     next($_POST);
+// // 	}
+// // 	ECHO "</FORM>";
+// // ?>
 
-<?php
-	ECHO "<FORM NAME=TRANSFER ACTION='../index.php/appy/proposal' METHOD=POST TARGET=pdfframe>";
-	while($element = current($_POST)) {
-    echo "<INPUT TYPE=HIDDEN NAME=".key($_POST)." VALUE='".$_POST[key($_POST)]."'>";
-    next($_POST);
-	}
-	ECHO "</FORM>";
-?>
-
-<center>
-<br>
+<!-- <!-- <center> --> -->
+<!-- <!-- <br> --> -->
 <input type="button" id="btn_print_1" value="列印提議文件" style="display:block;font-size:16" onclick="print_page();" /><br>
-<iframe id="pdfframe" name="pdfframe" src="dummy.html" width="800" height="800" scrolling="auto">
-</iframe>
-</center>
-<?php
+<!-- <!-- <iframe id="pdfframe" name="pdfframe" src="dummy.html" width="800" height="800" scrolling="auto"> --> -->
+<!-- <!-- </iframe> --> -->
+<!-- <!-- </center> --> -->
+// <?php
 
-?>
+// // ?>
 
-<script type="text/javascript">
-function print_page() {
-	window.frames["pdfframe"].focus();
-	window.frames["pdfframe"].print();
-}
-</script>
+<!-- <!-- <script type="text/javascript"> --> -->
+<!-- // function print_page() { -->
+<!-- // 	window.frames["pdfframe"].focus(); -->
+<!-- // 	window.frames["pdfframe"].print(); -->
+<!-- // } -->
+<!-- <!-- </script> --> -->
 
-</body>
-</html>
+<!-- <!-- </body> --> -->
+<!-- <!-- </html> --> -->
