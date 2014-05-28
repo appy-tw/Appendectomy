a163b4371caf65d4d880c49bf2823c187c05d7b3
cangming <cangming@ubuntu>
2014-05-29 01:02:08 +0800

    app大小寫更正

--- application/controllers/surgeryApp.php
+++ application/controllers/surgeryApp.php
@@ -24,7 +24,7 @@ class SurgeryApp extends CI_Controller
 				$PROCEED = false;
 			} else
 			{
-				$sql = "SELECT staff_id FROM staff_info WHERE NICKNAME=? AND PASSWORD=PASSWORD(?) AND level=?";
+				$sql = "SELECT staff_id FROM staff_info WHERE nickname=? AND password=PASSWORD(?) AND level=?";
 				$QUERY_STRING = $this->db->query ( $sql, array (
 						$NICKNAME,
 						$STFPWD,
@@ -42,7 +42,7 @@ class SurgeryApp extends CI_Controller
 			{
 				if ($STAFF == "")
 				{
-					$sql = "SELECT staff_id FROM staff_info WHERE NICKNAME=?";
+					$sql = "SELECT staff_id FROM staff_info WHERE nickname=?";
 					$QUERY_STRING = $this->db->query ( $sql, array (
 							$NICKNAME 
 					) );
@@ -56,16 +56,16 @@ class SurgeryApp extends CI_Controller
 					IF ($SNO [4] == 1)
 					{
 						
-						$MAIN_TABLE = "PROPOSAL";
-						$RECORD_TABLE = "PROPOSAL_CHANGE_RECORD";
+						$MAIN_TABLE = "proposal";
+						$RECORD_TABLE = "proposal_change_record";
 					} else IF ($SNO [4] == 2)
 					{
 						
-						$MAIN_TABLE = "PETITION";
-						$RECORD_TABLE = "PETITION_CHANGE_RECORD";
+						$MAIN_TABLE = "petition";
+						$RECORD_TABLE = "petition_change_record";
 					}
 					
-					$sql = "INSERT INTO ?(?_ID,STATUS_CHANGED_TO,STAFF_ID)VALUES(?,?,?)";
+					$sql = "INSERT INTO ?(?_id,status_changed_to,staff_id)VALUES(?,?,?)";
 					$QUERY_STRING = $this->db->query ( $sql, array (
 							$RECORD_TABLE,
 							$MAIN_TABLE,
@@ -76,7 +76,7 @@ class SurgeryApp extends CI_Controller
 					IF ($QUERY_STRING->num_rows () > 0)
 					{
 						$RECORD_ID = $this->db->mysql_insert_id ();
-						$sql = "SELECT current_status,id_last_five FROM ? WHERE ?_ID=? AND VALIDATION_CODE=?";
+						$sql = "SELECT current_status,id_last_five FROM ? WHERE ?_id=? AND validation_code=?";
 						$QUERY_STRING = $this->db->query ( $sql, array (
 								$MAIN_TABLE,
 								intval ( substr ( $SNO, 5 ) ),
@@ -94,7 +94,7 @@ class SurgeryApp extends CI_Controller
 									$sql = "";
 								} else
 								{
-									$sql = "UPDATE ? SET CURRENT_STATUS=?,ID_LAST_FIVE=? WHERE ?_ID=? AND VALIDATION_CODE=?";
+									$sql = "UPDATE ? SET current_status=?,id_last_five=? WHERE ?_id=? AND validation_code=?";
 									$QUERY_STRING = $this->db->query ( $sql, array (
 											$MAIN_TABLE,
 											$STATUS,
@@ -106,7 +106,7 @@ class SurgeryApp extends CI_Controller
 								}
 							} else
 							{
-								$sql = "UPDATE ? SET CURRENT_STATUS=? WHERE ?_ID=? AND VALIDATION_CODE=?";
+								$sql = "UPDATE ? SET current_status=? WHERE ?_id=? AND validation_code=?";
 								$QUERY_STRING = $this->db->query ( $sql, array (
 										$MAIN_TABLE,
 										$STATUS,
@@ -123,13 +123,13 @@ class SurgeryApp extends CI_Controller
 								{
 									IF ($this->db->affected_rows () == 1)
 									{
-										$sql = "UPDATE ? SET SUCCEED='1' WHERE ?_ID=?";
+										$sql = "UPDATE ? SET SUCCEED='1' WHERE ?_id=?";
 										$this->db->query ( $sql, array (
 												$RECORD_TABLE,
 												$RECORD_TABLE,
 												$RECORD_ID 
 										) );
-										$sql = "SELECT current_status,last_update FROM ? WHERE ?_ID=? AND VALIDATION_CODE=?";
+										$sql = "SELECT current_status,last_update FROM ? WHERE ?_id=? AND validation_code=?";
 										$QUERY_STRING = $this->db->query ( $sql, array (
 												$MAIN_TABLE,
 												$MAIN_TABLE,
