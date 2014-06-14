928eee0a0dc46f6942663b939df0b99b6d8aaee9
cangming <cangming@ubuntu>
2014-06-14 17:40:47 +0800

    立委條件變更

--- application/helpers/proposal_helper.php
+++ application/helpers/proposal_helper.php
@@ -29,11 +29,12 @@ if (! function_exists ( 'returnValidation' )) {
 
 if (! function_exists ( 'generatePDF' )) {
 	function generatePDF($pdf, $CHI_FONT, $ENG_FONT, $DATA, $NAME, $IDNo, $SEX, $BIRTHDAY, $OCCUPATION, $REGADD, $QRImgPath, $SNo, $PHONE) {		
-		if($DATA ['district_name'] == "廖正井"){
+		if($DATA ['district_legislator'] == "廖正井"){
 			CPRFormat($pdf, $CHI_FONT, $ENG_FONT, $DATA, $NAME, $IDNo, $SEX, $BIRTHDAY, $OCCUPATION, $REGADD, $QRImgPath, $SNo, $PHONE);
 		}
 		else{
-			appyFormat($pdf, $CHI_FONT, $ENG_FONT, $DATA, $NAME, $IDNo, $SEX, $BIRTHDAY, $OCCUPATION, $REGADD, $QRImgPath, $SNo, $PHONE);
+			appyFormat($
+			pdf, $CHI_FONT, $ENG_FONT, $DATA, $NAME, $IDNo, $SEX, $BIRTHDAY, $OCCUPATION, $REGADD, $QRImgPath, $SNo, $PHONE);
 		}
 	}
 }
